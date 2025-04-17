906af63519f3a844461e4e712da55deeb0df2131
qsgy-ubuntu <testemail@example.com>
2025-04-17 12:01:31 +0800

    完成协程调度模块

--- tests/test_scheduler.cc
+++ tests/test_scheduler.cc
@@ -3,14 +3,21 @@
 static qsgy::Logger::ptr g_logger = QSGY_LOG_ROOT();
 
 void test_fiber() {
-	QSGY_LOG_INFO(g_logger) << "test in fiber";
+	static int s_count = 5;
+	QSGY_LOG_INFO(g_logger) << "test in fiber s_count=" << s_count;
+	sleep(1);
+	if(--s_count >= 0) {
+		qsgy::Scheduler::GetThis()->schedule(&test_fiber, qsgy::GetThreadId());
+	}
 }
 
 int main(int argc, char** argv) {
 	QSGY_LOG_INFO(g_logger) << "main";
-	qsgy::Scheduler sc;
-	sc.schedule(&test_fiber);
+	qsgy::Scheduler sc(3, false, "test");
 	sc.start();
+	sleep(2);
+	QSGY_LOG_INFO(g_logger) << "scheduler";
+	sc.schedule(&test_fiber);
 	sc.stop();
 	QSGY_LOG_INFO(g_logger) << "over";
 	return 0;
